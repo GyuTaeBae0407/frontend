2d098b8ad32d022f0baff59bdce9f8e791967f4a
Gyu Tae Bae <50886917+GyuTaeBae0407@users.noreply.github.com>
2023-02-12 23:34:44 -0800

    Front
    Basic Login and Signup combined landing page

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  const handleClick = () => {
+    // implementation details
+  };
+  
+  return (
+    <div className="center-image">
+      <img 
+      style={{
+        height: 102,
+        width: 102,
+      }}
+      src={require('./logo.png')}/>
+    <div>
+
+      <div>
+        <button type="button" onClick={handleClick} className="login_button">
+        Login
+      </button>
+      <div>
+        <button type="button" onClick={handleClick} className="signup_button">
+        Sign Up
+      </button>
+      </div>
+  
+      
+      </div>
+    
+
+    </div>
+      <div className='Welcome-message' >Welcome to Toggle!</div>
+    </div>
+    
+    
+    
+  );
+
+}
+
+  
+export default App;
+
